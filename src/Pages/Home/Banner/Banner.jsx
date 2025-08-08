2477b2c99a456fffe9244ae332c78d53610bd246
bishalsaha-official <cse.bishalsaha@gmail.com>
2025-08-08 20:20:07 +0600

    Initial Project setup

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import logo from '../../../assets/home/banner.jpg'
+
+const Banner = () => {
+    return (
+        <div style={{backgroundImage: `url(${logo})`}} className='p-10 min-h-screen bg-center bg-no-repeat bg-cover'>
+            
+        </div>
+    );
+};
+
+export default Banner;
