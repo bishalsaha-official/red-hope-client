b0a9f04264e82a7b3bf946e60b3c0770652f5618
bishalsaha-official <cse.bishalsaha@gmail.com>
2025-08-08 21:28:20 +0600

    Implement banner in home page

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,9 +1,28 @@
+import { Typewriter } from 'react-simple-typewriter';
 import logo from '../../../assets/home/banner.jpg'
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
-        <div style={{backgroundImage: `url(${logo})`}} className='p-10 min-h-screen bg-center bg-no-repeat bg-cover'>
-            
+        <div style={{ backgroundImage: `url(${logo})` }} className='p-10 min-h-screen bg-center bg-no-repeat bg-cover flex items-center'>
+            <div className='max-w-3xl mx-auto text-white text-center'>
+                <h1 className='font-bold text-5xl mb-5'>
+                    <Typewriter
+                        words={['Donate Blood, Save Lives', 'Every Drop Counts', 'Join the RedHope Community']}
+                        loop={0}
+                        cursor
+                    ></Typewriter>
+                </h1>
+                <p className='text-xl'>RedHope is a community-driven platform dedicated to making blood donation faster, safer, and more accessible. Whether you're looking to donate or in urgent need, we're here to connect lives with compassion and hope.</p>
+                <div className='mt-5'>
+                    <Link>
+                        <button className='btn mr-3 bg-[#D32F2F] text-white border-0 px-10 py-5'>Join as a donor</button>
+                    </Link>
+                    <Link>
+                        <button className='btn bg-white text-[#D32F2F] border-0 px-10 py-5'>Search Donors</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
