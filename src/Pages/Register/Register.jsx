707e1cb047a96b473337c2b0133e7fcb5ce083fb
bishalsaha-official <cse.bishalsaha@gmail.com>
2025-08-10 15:38:45 +0600

    design register and login page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,27 +1,115 @@
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Await, Link, useNavigate } from "react-router-dom";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import useAuth from "../../Hooks/useAuth";
+import Swal from "sweetalert2";
+import donor from '../../assets/register/donor.jpg'
 
 const Register = () => {
-    const { register, handleSubmit, watch, reset, formState: { errors } } = useForm();
+    const [upazilas, setUpazilas] = useState([])
+    const [districts, setDistricts] = useState([])
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const password = watch("password");
+    const axiosPublic = useAxiosPublic()
+    const image_hosting_key = import.meta.env.VITE_Image_Hosting_Key;
+    const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
+    const { createUser, updateUserProfile } = useAuth()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        fetch('/public/upazilas.json')
+            .then(res => res.json())
+            .then(data => {
+                setUpazilas(data[2].data)
+            })
+    }, [])
+
+    useEffect(() => {
+        fetch('/public/districts.json')
+            .then(res => res.json())
+            .then(data => {
+                setDistricts(data[2].data)
+            })
+    }, [])
+
     const onSubmit = async (data) => {
         console.log(data)
+        // image hosting
+        const imageFile = { image: data.image[0] }
+        const imgRes = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
+        })
+        if (!imgRes.data.success) {
+            Swal.fire({
+                icon: "error",
+                title: "Image upload failed",
+                text: "Please try again."
+            });
+            return;
+        }
+        const imageUrl = imgRes.data.data.display_url;
+
+        // create user
+        createUser(data.email, data.password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser)
+                // update profile
+                updateUserProfile(data.name, imageUrl)
+                    .then(async () => {
+                        console.log('Updated Profile')
+                        // Send User Information to the database
+                        const userInformation = {
+                            email: data.email,
+                            name: data.name,
+                            image: imageUrl,
+                            bloodGroup: data.bloodGroup,
+                            district: data.district,
+                            upazila: data.upazila,
+                            role: "donor",
+                            status: "active"
+                        };
+                        const res = await axiosPublic.post('/users', userInformation);
+                        console.log("Backend Response:", res.data);
+                        if (res.data.insertedId) {
+                            Swal.fire({
+                                position: "top-center",
+                                icon: "success",
+                                title: "Donor registered successfully",
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                            navigate('/');
+                        }
+                    })
+            })
+            .catch(error => {
+                console.log(error)
+                Swal.fire({
+                    icon: "error",
+                    title: "Something went wrong",
+                    text: error.message
+                });
+            })
     }
 
+
     return (
-        <section className="min-h-screen flex items-center justify-center bg-cover bg-center p-4"
-            style={{ backgroundImage: "linear-gradient(rgba(239, 61, 50, 0.8), rgba(239, 61, 50, 0.8)), url('https://images.unsplash.com/photo-1604909052951-6248bda01018')" }}>
-            <div className="max-w-5xl bg-white rounded-lg shadow-lg overflow-hidden grid grid-cols-1 md:grid-cols-2">
+        <section className="min-h-screen flex items-center justify-center p-4 bg-[#FAF3E0]">
+            <div className="max-w-5xl bg-[#F7F7F7] rounded-lg shadow-lg overflow-hidden grid grid-cols-1 md:grid-cols-2">
                 {/* Left side image */}
                 <div className="hidden md:block">
-                    <img src="https://images.unsplash.com/photo-1582719478250-c89cae4dc85b"
+                    <img src={donor}
                         alt="Donate Blood"
                         className="w-full h-full object-cover" />
                 </div>
 
                 {/* Right side form */}
                 <div className="p-6 md:p-10">
-                    <h2 className="text-2xl font-bold text-[#EF3D32] mb-4 text-center">Register</h2>
+                    <h2 className="text-2xl font-bold text-[#EF3D32] mb-4 text-center">Join as a Donor</h2>
                     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
                         {/* Name */}
                         <input type="text" placeholder="Full Name" {...register("name", { required: true })} className="input input-bordered w-full" />
@@ -32,8 +120,8 @@ const Register = () => {
                         {errors.email && <p className="text-[#EF3D32] text-sm">Email is required</p>}
 
                         {/* Photo */}
-                        <input type="file" {...register("photo", { required: true })} className="file-input file-input-bordered w-full" />
-                        {errors.photo && <p className="text-[#EF3D32] text-sm">Photo is required</p>}
+                        <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered w-full file-input-secondary" />
+                        {errors.image && <p className="text-[#EF3D32] text-sm">Photo is required</p>}
 
                         {/* Blood Group */}
                         <select {...register("bloodGroup", { required: true })} className="select select-bordered w-full">
@@ -45,18 +133,18 @@ const Register = () => {
                         {/* District */}
                         <select {...register("district", { required: true })} className="select select-bordered w-full">
                             <option value="">Select District</option>
-                            <option>Dhaka</option>
-                            <option>Chattogram</option>
-                            <option>Khulna</option>
+                            {
+                                districts.map(district => <option key={district.id}>{district.name}</option>)
+                            }
                         </select>
                         {errors.district && <p className="text-[#EF3D32] text-sm">District is required</p>}
 
                         {/* Upazila */}
                         <select {...register("upazila", { required: true })} className="select select-bordered w-full">
                             <option value="">Select Upazila</option>
-                            <option>Mirpur</option>
-                            <option>Gulshan</option>
-                            <option>Banani</option>
+                            {
+                                upazilas.map(upazila => <option key={upazila.id}>{upazila.name}</option>)
+                            }
                         </select>
                         {errors.upazila && <p className="text-[#EF3D32] text-sm">Upazila is required</p>}
 
@@ -85,10 +173,10 @@ const Register = () => {
                         {errors.confirm_password && <p className="text-[#EF3D32] text-sm">Passwords do not match</p>}
 
                         <button type="submit" className="w-full bg-[#EF3D32] hover:bg-red-700 text-white py-2 rounded">
-                            register
+                            Register
                         </button>
                     </form>
-                    <p className="text-center my-5">Already have as donor? <Link className="text-[#EF3D32]" to='/login'>Login</Link></p>
+                    <p className="text-center my-5">Already registered as a donor? <Link className="text-[#EF3D32]" to='/login'>Login</Link></p>
                 </div>
             </div>
         </section>
--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,8 @@
-import { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 import { app } from "../Firebase/firebase.config";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
+
 
 export const AuthContext = createContext(null)
 const auth = getAuth(app)
@@ -8,10 +10,60 @@ const auth = getAuth(app)
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState([])
     const [loading, setLoading] = useState(true)
+    const axiosPublic = useAxiosPublic()
+
+    // Create User
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // Login User
+    const loginUser = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // LogOut User
+    const logoutUser = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    // Update Profile
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+        })
+    }
+
+    // Observer
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser)
+            const userInfo = { email: currentUser?.email }
+            if (currentUser) {
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        localStorage.setItem('access-toke', res.data.token)
+                    })
+            }
+            else {
+                localStorage.removeItem('access-token')
+            }
+        })
+        return () => {
+            unSubscribe()
+        }
+    }, [axiosPublic])
 
     const authInfo = {
         user,
-        loading
+        loading,
+        createUser,
+        loginUser,
+        logoutUser,
+        updateUserProfile
     }
 
     return (
